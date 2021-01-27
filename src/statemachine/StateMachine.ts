0efb08e7a673f86c3441cb51bc3aaceea96dd082
WKQ <wenkangqiang.wkq@alibaba-inc.com>
2021-01-27 13:34:28 +0800

    feat: statemachine

--- src/statemachine/StateMachine.ts
+++ src/statemachine/StateMachine.ts
@@ -1,4 +1,4 @@
-interface StateConfig
+interface IState 
 {
     name?: string
     onEnter?: () => void
@@ -6,21 +6,35 @@ interface StateConfig
     onExit?: () => void
 }
 
+let idCount = 0;
 
 export default class StateMachine
 {
+    private id = (++idCount).toString()
+    private context?: object
+    private states = new Map<string, IState>()
 
-    private context?: any
-    private name: string
-    private states = new Map<string, StateConfig>()
+    private previousState?: IState
+    private currentState?: IState
+    private isChangingState = false
+    private changeStateQueue: string[] = []
 
-    private currentState?: StateConfig
 
-    constructor(context?: any, name?: string)
+    get previousStateName()
     {
-        this.context = context
-        this.name = name || "default"
+        if (!this.previousState)
+        {
+            return ''
+        }
+        return this.previousState.name
     }
+
+    constructor(context?: object, id?: string)
+    {
+        this.id = id ?? this.id
+        this.context = context
+    }
+
 
     isCurrentState(name: string)
     {
@@ -31,7 +45,7 @@ export default class StateMachine
         return this.currentState.name === name
     }
 
-    addState(name: string, config?: StateConfig)
+    addState(name: string, config?: IState)
     {
         this.states.set(name, {
             name,
@@ -46,31 +60,51 @@ export default class StateMachine
     {
         if (!this.states.has(name))
         {
+            console.warn(`Tried to change to unknown state ${name}`)
             return
         }
+
+        if (this.isCurrentState(name))
+        {
+            return
+        }
+
+        if (this.isChangingState) 
+        {
+            this.changeStateQueue.push(name)
+            return
+        }
+
+        this.isChangingState = true
+
+        console.log(`StateMachine (${this.id})] change from ${this.currentState?.name ?? 'none'} to ${name}`)
 
         if (this.currentState && this.currentState.onExit)
         {
             this.currentState.onExit()
         }
 
+        this.previousState = this.currentState
         this.currentState = this.states.get(name)!
         if (this.currentState.onEnter)
         {
             this.currentState.onEnter()
         }
+
+        this.isChangingState = false
     }
 
     update(dt: number)
     {
-        if (!this.currentState)
-        {
-            return
-        }
+       if (this.changeStateQueue.length > 9)
+       {
+           this.setState(this.changeStateQueue.shift()!)
+           return
+       }
 
-        if (this.currentState.onUpdate)
-        {
-            this.currentState.onUpdate(dt)
-        }
+       if (this.currentState && this.currentState.onUpdate)
+       {
+           this.currentState.onUpdate(dt)
+       }
     }
 }
